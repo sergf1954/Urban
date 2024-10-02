97482bcdab69f8174726fecb82b7aa861fbcd715
sergf1954 <147498212+sergf1954@users.noreply.github.com>
2024-10-02 22:58:00 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# "1st program"
+print((9**0.5)*5)
+
+# "2nd program"
+print((9.99 > 9.8) and (1000 != 1000.1))
+
+# "3rd program"
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print( (2 * 2 + 2) == (2 * (2 + 2)) )
+
+# "4th program"
+name ='123.456'
+num = float(name)
+num = num * 10
+num = int(num)
+num = num % 10
+print(num)
+#print(name[4:5]) это дает тот же результат
