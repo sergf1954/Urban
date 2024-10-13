c41d22ab84b0a6b50a6f2deece708ac15516dba3
sergf1954 <147498212+sergf1954@users.noreply.github.com>
2024-10-13 19:21:16 +0400

    Add files via upload

---
+++ module_3_4.py
@@ -0,0 +1,27 @@
+def single_root_words(root_word, *other_words):
+    same_words  = []
+
+    for i in  other_words:
+
+        if str(i).upper().count(root_word.upper()) != 0:
+           same_words.append(i)
+
+    if len(same_words) != 0:
+        return same_words
+
+    for i in other_words:
+
+         if str(i).upper() in root_word.upper():
+            same_words.append(i)
+
+    return same_words
+
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+
+
+print(result1)
+
+print(result2)
