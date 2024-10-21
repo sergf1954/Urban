3f4a729e3b114f351ef1d025d9f43d6f70c9f34b
sergf1954 <147498212+sergf1954@users.noreply.github.com>
2024-10-21 17:49:26 +0400

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,4 +1,5 @@
 def send_email(message, recipient, *, sender='university.help@gmail.com'):
+
     sender_default = 'university.help@gmail.com'
 
     if recipient == sender:
@@ -10,21 +11,24 @@ def send_email(message, recipient, *, sender='university.help@gmail.com'):
         return message + sender + 'на адрес' + recipient
     if "@" not in recipient:
         #print('Нет собаки')
-        message = 'Невозможно отправить письмо на адрес '
-        return message + sender + 'на адрес' + recipient
+        message = 'Невозможно отправить письмо c адреса '
+        return message + sender + 'на адрес ' + recipient
 
+    flag_sender = 0 # 1 нет совпадений  2 отправитель не стандартный
 
     for i in ('.com', '.net', '.ru'):
-
+        perem = sender[-len(i):]
+        if i not in sender[-len(i):]:
+            flag_sender = 1
+        else:
+            flag_sender = 0
         if i in sender[-len(i):]:
             if i == '.com' and sender == sender_default:
                 break
             else:
-                message = 'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса '
-                return message + sender + ' на адрес ' + recipient
-        else:
-            message = 'Невозможно отправить письмо с адреса '
-            return message + sender + ' на адрес ' + recipient
+                flag_sender = 2
+                break
+
 
     flag = 0  # 1 - нет совпадений
     for j in ('.com', '.ru', '.net'):
@@ -35,7 +39,7 @@ def send_email(message, recipient, *, sender='university.help@gmail.com'):
             flag = 0
             break
 
-    if flag == 0:
+    if flag == 0 and flag_sender == 0:
         if j in recipient[-len(j):]:
             if j == '.com':
                 message ='Письмо успешно отправлено с адреса '
@@ -43,10 +47,17 @@ def send_email(message, recipient, *, sender='university.help@gmail.com'):
             else:
                 message = 'НЕСТАНДАРТНЫЙ ПОЛУЧАТЕЛЬ! Письмо отправлено с адреса '
                 return message + sender + ' на адрес '+ recipient
-                    #return message
 
     if flag == 1:
         message = 'Невозможно 111 11 отправить письмо c адреса '
+        return message + sender + ' на адрес ' + recipient
+
+    if flag_sender == 2:
+        message = 'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса  '
+        return message + sender + ' на адрес ' + recipient
+
+    if flag_sender == 1:
+        message = 'Невозможно отправить письмо с адреса '
         return message + sender + ' на адрес ' + recipient
 
 print(send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com' ))
@@ -57,7 +68,7 @@ print(send_email('Пожалуйста, исправьте задание','urba
 
 print(send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru'))
 
-print(send_email('Пожалуйста, исправьте задание','urban.student@mail.ru'))
+print(send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='university.help@gmail.com'))
 
 
 
