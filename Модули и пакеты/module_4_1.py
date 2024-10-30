4c70dceaffd24a10ee88ec74a759bbb14416bf9a
sergf1954 <147498212+sergf1954@users.noreply.github.com>
2024-10-30 19:50:02 +0400

    Add files via upload

---
+++ Модули и пакеты/module_4_1.py
@@ -0,0 +1,16 @@
+import fake_math as m1
+import  true_math as m2
+
+
+result1 = m1.divide(69, 3)
+result2 = m1.divide(3, 0)
+result3 = m2.divide(49, 7)
+result4 = m2.divide(15, 0)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+
+
+
