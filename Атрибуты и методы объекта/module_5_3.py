7a096ab92494341699232048b8652c0bb359bbb2
sergf1954 <147498212+sergf1954@users.noreply.github.com>
2024-11-06 17:52:24 +0400

    Add files via upload

--- Атрибуты и методы объекта/module_5_3.py
+++ Атрибуты и методы объекта/module_5_3.py
@@ -1,18 +1,11 @@
 #    Домашняя работа по уроку "Перегрузка операторов"
-from ctypes.wintypes import PRECT
-
 
 class House:
+
     def __init__(self, name='ЖК Эльбрус',number_of_floors=30):
         self.number = number_of_floors
         self.name   = name
 
-
-    def __eq__(self, other):
-        if isinstance(other,House):
-            return self.number == other.number
-        else:
-            return 'Не знаю что это '
 
     def __len__(self):
         if isinstance(self.number, int):
@@ -20,24 +13,71 @@ class House:
         else:
             return False
     def __str__(self):
-
         return f'Название: {self.name} кол-во этажей {self.number}'
-##################################################
-    # def __add__(self, other):
-    #     if isinstance(other, int):
-    #         return self.number + other
-    #     else:
-    #         return self.number
+
 #################################################################
+
+    ############################
     def __add__(self, other):
+        #print("Print __add__  CALL")
         if isinstance(other, House):
-            return House(self.number + other.number)
-        else:
-            return House(self.number + other)
+            self.number = self.number + other.number
+
+            return self
+        if isinstance(other, int):
+           self.number = self.number + other
+           return self
+        raise NotImplemented
+
     def __radd__(self, other):
-        #ff= self.number
-        return self.__add__(other)
-################################################################
+        #print("Print __radd__  CALL")
+        return self + other
+    #########################  __iadd__
+    def __iadd__(self, other):
+
+        if isinstance(other, House):
+            self.number += other.number
+            return self
+        if isinstance(other, int):
+            self.number += other
+            return self
+        raise NotImplemented
+
+    #############################
+    ##########        Сравнение   ##########
+    def __eq__(self, other):
+        if other is None or not isinstance(other, House):
+            return False
+        return self.number == other.number
+        #   больше
+    def __gt__(self, other):
+        if other is None or not isinstance(other, House):
+            return False
+        return self.number > other.number
+        # больше равно
+    def __ge__(self, other):
+        if other is None or not isinstance(other, House):
+            return False
+        return self.number >= other.number
+
+        # меньше
+    def __lt__(self, other):
+        if other is None or not isinstance(other, House):
+            return False
+        return self.number < other.number
+
+        # меньше равно
+    def __le__(self, other):
+        if other is None or not isinstance(other, House):
+            return False
+        return self.number <= other.number
+        # не равно
+    def __ne__(self, other):
+        if other is None or not isinstance(other, House):
+            return False
+        return self.number != other.number
+
+    ################################################################
     def go_to(self, new_floor):
         if 0 <= new_floor < self.number:
             for i in range(new_floor):
@@ -45,19 +85,29 @@ class House:
         else:
             print (f'Такого этажа не существует')
 
-
-
+##############################
 h1 = House('ЖК Эльбрус', 10)
-h2 = House('ЖК Акация', 15)
-
-#h1.go_to(5)
-#h2.go_to(25)
+h2 = House('ЖК Акация', 20)
 
 print(h1)
-#print(h2)
+print(h2)
 
-#print(h1 == h2)  # __eq__
+print(h1 == h2) # __eq__
 
-h1 = h1 + 5  # __add__
+h1 = h1 + 10 # __add__
+
 print(h1)
-#print(h1 == h2)  # __eq__
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
+##############################
