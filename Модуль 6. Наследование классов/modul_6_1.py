bc0b083ba6928b4fb42e4b29459a7da0ac82d487
sergf1954 <147498212+sergf1954@users.noreply.github.com>
2024-11-12 15:58:22 +0400

    Add files via upload

--- Модуль 6. Наследование классов/modul_6_1.py
+++ Модуль 6. Наследование классов/modul_6_1.py
@@ -1,1 +1,57 @@
+#    Задача "Съедобное, несъедобное":
 
+class Animal:
+    alive = True                # (живой)
+    fed   = False               # (накормленный),
+
+    def __init__(self, name):
+        self.name = name         # - индивидуальное  название  каждого  животного
+
+
+class Plant:
+    edible = False                # (съедобность),
+
+    def __init__(self, name):
+        self.name = name          # name - индивидуальное  название каждого  растения
+
+#    Start наследники для Animal
+class Mammal(Animal):
+
+    def eat(self, food):
+        print(self.name, 'съел ', food.name)
+        self.fed = True
+
+
+class Predator(Animal):                # a1 - Predator
+
+
+    def eat(self, food):
+        print(self.name, 'не стал есть ', food.name)
+        self.alive = False
+
+#    End наследники для Animal
+
+#   Start   наследники для Plant
+class Flower(Plant):                 # p1 для a1
+    edible = False                   # (съедобность)
+
+
+class Fruit(Plant):
+    edible = True
+
+#   End  наследники для Plant
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик самоцветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
