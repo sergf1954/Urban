d94005c8dde5c6d045b0b13a89f365746bca325f
sergf1954 <147498212+sergf1954@users.noreply.github.com>
2024-11-25 19:48:57 +0400

    Add files via upload

--- Модуль8/module_8_1.py
+++ Модуль8/module_8_1.py
@@ -1,1 +1,22 @@
 
+def add_everything_up(a, b):
+    try:
+        return round((a + b),3)
+    except TypeError as exc:
+        if type(a) == float and type(b) == str:
+            return str(a) + b
+        elif type(a) == str and type(b) == int:
+            return a + str(b)
+        else:
+            return f' Hello {exc}'
+
+    #
+    # finally:
+    #     print(' finally Завершили урок')
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
+print(add_everything_up('123.456', '7'))
+
+
