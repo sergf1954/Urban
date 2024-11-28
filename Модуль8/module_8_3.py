039ace9616c1b5cc23c7200b3ad7190ca00cf6d4
sergf1954 <147498212+sergf1954@users.noreply.github.com>
2024-11-28 22:51:25 +0400

    Add files via upload

--- Модуль8/module_8_3.py
+++ Модуль8/module_8_3.py
@@ -11,12 +11,12 @@ class IncorrectCarNumbers(Exception):
 
 
 class Car:
-    def __init__(self, model, __vin, numbers):
+    def __init__(self, model:str, __vin:int, __numbers:str):
         self.model   = model
         if self.__is_valid_vin(__vin):
             self.__vin = __vin
-        if self.__is_valid_numbers(numbers):
-            self.__numbers = numbers
+        if self.__is_valid_numbers(__numbers):
+            self.__numbers = __numbers
 
 
     def __is_valid_vin(self,vin_number):
