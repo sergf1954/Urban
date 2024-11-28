23527b4661beb6a2b3c7aeffe6f457f0c55d05a9
sergf1954 <147498212+sergf1954@users.noreply.github.com>
2024-11-28 20:07:15 +0400

    Add files via upload

--- Модуль8/module_8_1.py
+++ Модуль8/module_8_1.py
@@ -1,22 +1,26 @@
 
 def add_everything_up(a, b):
     try:
-        return round((a + b),3)
+        #return round((a + b),3)
+        return a + b
     except TypeError as exc:
-        if type(a) == float and type(b) == str:
+
+        if isinstance(a, float) and isinstance(b,str):
             return str(a) + b
-        elif type(a) == str and type(b) == int:
+        elif isinstance(a,str) and isinstance(b, int):
             return a + str(b)
+
+
         else:
             return f' Hello {exc}'
 
-    #
+
     # finally:
-    #     print(' finally Завершили урок')
+    #    print(' finally Завершили урок')
 
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
 print(add_everything_up(123.456, 7))
-print(add_everything_up('123.456', '7'))
+#print(add_everything_up([123.456], 7))
 
 
