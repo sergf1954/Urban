65c01fd7f5698d026b080c9ad74b1d0b580f3cce
sergf1954 <147498212+sergf1954@users.noreply.github.com>
2024-11-30 21:53:28 +0400

    Add files via upload

--- Модуль9/modul_9_1.py
+++ Модуль9/modul_9_1.py
@@ -3,7 +3,7 @@
 def apply_all_func(int_list, *function):
     results  ={}
     for i in function:
-        results .update({i.__name__: i(int_list)})
+        results.update({i.__name__: i(int_list)})
     return results
 
 print(apply_all_func([6, 20, 15, 9],min, max),end=' ')
