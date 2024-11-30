08704d9accb9d93b58b7ef1debb841419631765d
sergf1954 <147498212+sergf1954@users.noreply.github.com>
2024-11-30 21:51:10 +0400

    Add files via upload

--- Модуль9/modul_9_1.py
+++ Модуль9/modul_9_1.py
@@ -1,1 +1,12 @@
 
+
+def apply_all_func(int_list, *function):
+    results  ={}
+    for i in function:
+        results .update({i.__name__: i(int_list)})
+    return results
+
+print(apply_all_func([6, 20, 15, 9],min, max),end=' ')
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
+
+
