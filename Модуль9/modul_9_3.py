a0bf76c6cd28e06e55e8cea3a6e9cda371a5cd5d
sergf1954 <147498212+sergf1954@users.noreply.github.com>
2024-12-02 21:39:37 +0400

    Add files via upload

---
+++ Модуль9/modul_9_3.py
@@ -0,0 +1,22 @@
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+first_result = (len(x)-len(y) for x,y in zip(first, second) if len(x) - len(y))
+#
+second_result = ((bool(len(first[i]) == len(second[i])))
+          if (len(first[i]) == len(second[i])) else (len(first[i]) == len(second[i]))
+          for i in range(len(first)))
+
+
+print(list(first_result))
+
+print(list(second_result))
+
+
+
+# В переменную second_result запишите генераторную сборку,
+# которая содержит результаты сравнения длин строк
+# в одинаковых позициях из списков first и second.
+# Составьте эту сборку НЕ используя функцию zip.
+# Используйте функции range и len.
+#
