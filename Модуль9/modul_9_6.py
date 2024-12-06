0f1b9a0af2f60b283a0123438e50c1fdc8a86f57
sergf1954 <147498212+sergf1954@users.noreply.github.com>
2024-12-06 19:43:59 +0400

    Add files via upload

--- Модуль9/modul_9_6.py
+++ Модуль9/modul_9_6.py
@@ -1,13 +1,20 @@
 def all_variants(text):
     for i in text:
         yield i
-    for j in range(len(text)):
-        if j < len(text) - 1:
-            yield text[j:j+2]
-        else:
-            yield text
+        
+    n = 2
+    while n < len(text):
+        for j in range(len(text)):
+            if j < len(text) - 1 and j+n-1 < len(text):
+                yield text[j:j+n]
+            else:
+                continue
 
-a = all_variants('ABCDE')
+        n = n + 1
+        if n == len(text):
+            yield text
+            break
+a = all_variants('ABCDEF')
 
 for i in a:
     print(i)
