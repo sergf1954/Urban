3c66242b09c7a24b37718bd8f09775ba174f682c
sergf1954 <147498212+sergf1954@users.noreply.github.com>
2024-12-09 18:30:44 +0400

    Add files via upload

--- Модуль9/modul_9_7.py
+++ Модуль9/modul_9_7.py
@@ -2,13 +2,11 @@
 # Задание: Декораторы в Python
 
 def isprime(n):
+    d = 2
+    while d * d <= n and n % d != 0:
+         d += 1
+    return d * d > n
 
-    # простые числа заканчиваются всегда на 1,3,7,9
-    if (((str(n))[-1]) == '1' or ((str(n))[-1]) == '3'
-            or ((str(n))[-1]) == '7' or ((str(n))[-1]) == '9'):
-        return True
-    else:
-        return False
 
 def is_prime(func):
     def wrapper(*numbers):
@@ -49,3 +47,10 @@ print(result)
 #      d += 1
 #  return d * d > n
 
+# простые числа заканчиваются всегда на 1,3,7,9
+# Да есть исключение цифры 2 и 5
+    # if (((str(n))[-1]) == '1' or ((str(n))[-1]) == '3'
+    #         or ((str(n))[-1]) == '7' or ((str(n))[-1]) == '9'):
+    #     return True
+    # else:
+    #     return False
