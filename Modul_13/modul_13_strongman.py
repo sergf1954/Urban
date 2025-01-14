181cd7ef8957834853e46b69986ed7d4af18c6e7
sergf1954 <147498212+sergf1954@users.noreply.github.com>
2025-01-14 16:50:53 +0400

    Add files via upload

--- Modul_13/modul_13_strongman.py
+++ Modul_13/modul_13_strongman.py
@@ -1,1 +1,25 @@
+import asyncio
 
+
+async def start_strongman(name, power):
+    ball = 1
+    Number_of_balls = 5
+    print(f"Силач - {name} начал соревнование")
+    while ball < 6:
+        y = Number_of_balls / power
+        await asyncio.sleep(y)
+        print(f'Силач {name} поднял шар {ball}')
+        ball +=1
+    print(f"Силач - {name} закончил соревнование")
+
+
+async def start_tournament():
+    task_1 = asyncio.create_task(start_strongman('Pasha', 3))
+    task_2 = asyncio.create_task(start_strongman('Denis', 4))
+    task_3 = asyncio.create_task(start_strongman('Apollon', 5))
+    await task_1
+    await task_2
+    await task_3
+
+
+asyncio.run(start_tournament())
